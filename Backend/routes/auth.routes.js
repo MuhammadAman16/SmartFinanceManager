195efc1b9bcc8cafb7a1fb82830dff8256856cf4
Syed Mujtaba <127235152+SyedMujtaba20@users.noreply.github.com>
2024-09-15 01:24:39 +0500

    test

--- Backend/routes/auth.routes.js
+++ Backend/routes/auth.routes.js
@@ -1,11 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authController = require('../controller/auth.controller');
+const authController = require("../controller/auth.controller");
 // Sample login route
-router.post('/login', authController.login);
+router.post("/login", authController.login);
 
-
-router.post('/signup', authController.signup);
-
+router.post("/signup", authController.signup);
 
 module.exports = router;
