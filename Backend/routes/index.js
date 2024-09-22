4e96dd61e277c7c965c97e2a592778ca2664ff5a
Syed Mujtaba <127235152+SyedMujtaba20@users.noreply.github.com>
2024-09-23 00:32:00 +0500

    budget crud

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,9 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const authRoutes = require('./auth.routes');
+const authRoutes = require("./auth.routes");
+const budgetRoutes = require("./budget.routes");
 
-router.use('/auth', authRoutes);
-
+router.use("/auth", authRoutes);
+app.use("/budgets", budgetRoutes);
 
 module.exports = router;
