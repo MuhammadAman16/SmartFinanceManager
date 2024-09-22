64f721621a0b78d56d7c6a9f06fbe616db01c471
Syed Mujtaba <127235152+SyedMujtaba20@users.noreply.github.com>
2024-09-23 00:38:03 +0500

    add index.js

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -5,8 +5,10 @@ const authRoutes = require("./auth.routes");
 const categoryRoutes = require("./categories.routes");
 const labelRoutes = require("./label.routes");
 
+const budgetRoutes = require("./budget.routes");
 router.use("/auth", authRoutes);
 router.use("/category", categoryRoutes);
 router.use("/label", labelRoutes);
+router.use("/budget", budgetRoutes);
 
 module.exports = router;
