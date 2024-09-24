86571319a98b27ff886bda78e86fba27035950f2
Syed Mujtaba <127235152+SyedMujtaba20@users.noreply.github.com>
2024-09-24 23:20:22 +0500

    category crud

--- Backend/controller/categories.controller.js
+++ Backend/controller/categories.controller.js
@@ -1,20 +1,20 @@
 // controller/categories.controller.js
-const { Category } = require('../models'); // Assuming your model is named Category
-const { errorHandler } = require('../utils/errorHandler');
+const { Category } = require("../models"); // Assuming your model is named Category
+const { errorHandler } = require("../utils/errorHandler");
 
 exports.getAllCategories = async (req, res, next) => {
-    try {
-        // Get all categories from the database
-        const categories = await Category.findAll();
+  try {
+    // Get all categories from the database
+    const categories = await Category.findAll();
 
-        // Return the categories in the response
-        return res.status(200).json({
-            message: 'Categories retrieved successfully',
-            categories
-        });
-    } catch (error) {
-        console.log("error", error);
-        // Pass the error to the error handling middleware
-        next(errorHandler(500, 'Error retrieving categories!'));
-    }
+    // Return the categories in the response
+    return res.status(200).json({
+      message: "Categories retrieved successfully",
+      data: categories,
+    });
+  } catch (error) {
+    console.log("error", error);
+    // Pass the error to the error handling middleware
+    next(errorHandler(500, "Error retrieving categories!"));
+  }
 };
