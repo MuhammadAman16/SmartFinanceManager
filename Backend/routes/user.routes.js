221e50b361ecdf16cdb0f8f6770fcd52870db6a6
Shaheryar-ahmed-154 <shaheryar.ahmed.ten@gmail.com>
2025-01-11 16:43:34 +0500

    webhook

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -12,4 +12,14 @@ router.put("/updatePassword", userController.updatePassword);
 
 router.post("/message", userController.sendMessage)
 
+router.post('/whatsapp-webhook', (req, res) => {
+    const { From, Body } = req.body; // Extract sender and message content
+    console.log("req.body", req.body);
+    console.log(`Message received from ${From}: ${Body}`);
+
+    // Respond to Twilio
+    res.set('Content-Type', 'text/xml');
+    res.send('<Response></Response>'); // Empty response for Twilio
+});
+
 module.exports = router;
