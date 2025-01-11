82eac010b927c13fe175507fb0ce5a8282522660
Shaheryar-ahmed-154 <shaheryar.ahmed.ten@gmail.com>
2025-01-12 00:23:56 +0500

    wip

---
+++ Backend/services/messaging.service.js
@@ -0,0 +1,15 @@
+exports.sendWhatsAppMessage = async (phoneNumber, message) => {
+    const accountSid = 'ACa8f41f9830e890f8260be0c610577d03';
+    const authToken = '483b8f58314890e0dec0af4cac90266e';
+
+    const client = require('twilio')(accountSid, authToken);
+    message = 'awdawCongrats, awdwyour request is succesfully processed!'
+
+
+    return client.messages
+        .create({
+            body: message,
+            from: 'whatsapp:+14155238886',
+            to: `whatsapp:${phoneNumber}`
+        })
+}
