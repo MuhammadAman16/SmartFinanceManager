84e2f342d62dac4de00bf93a8e12e21aa39bfc91
Muhammad Aman <mohammedaman5253@gmail.com>
2025-01-12 00:39:40 +0500

    bug fix

--- Backend/controller/chatbot.controller.js
+++ Backend/controller/chatbot.controller.js
@@ -222,7 +222,6 @@ Query:
       }
     } else if (trimmedResult == "update_name") {
       await getParams(updateNamePrompt);
-      console.log(params);
       if (params.fullName) {
         req.body = { userId: req.user.id, ...params };
         data = await updateFullName(req, res, next);
@@ -238,7 +237,13 @@ Query:
       const result = await model.generateContent(query);
       data = res.json({ response: result.response.text() });
     }
-    return res.status(200).send(data);
+    if (
+      trimmedResult == "get_u_pass" ||
+      trimmedResult == "get_u_email" ||
+      trimmedResult == "get_u_name"
+    ) {
+      return res.status(200).send(data);
+    }
   } catch (error) {
     console.error("Chatbot error please try again:", error);
     return res.status(500);
